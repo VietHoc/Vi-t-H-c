d99b1f847d48858078a777de7c819839700bdadc
naruto <viethoc@naruto.net.fpt>
2017-02-16 09:02:54 +0700

    oke

--- cheese/app.rb
+++ cheese/app.rb
@@ -19,11 +19,11 @@ end
 
 # -READ ALL: index -
 get '/cheeses' do
-	@cheeses = Cheese.all
+	@cheeses=Cheese.all
 	erb :'cheeses/index'
 end
-#
-#
+
+
 #- FORM for CREATING: new -
 get '/cheeses/new' do
 	erb :'cheeses/new'
@@ -37,7 +37,7 @@ end
 
 # -READ INDIVIDUAL: show -
 get '/cheeses/:id' do
-#	@cheese = Cheese.find(params[:id])
+	@cheese = Cheese.find(params[:id])
 	erb :'cheeses/show'
 end
 
@@ -49,15 +49,15 @@ get '/cheeses/:id/edit' do
 end
 # -UPDATE CHEESE: update -
 put '/cheeses/:id' do
-#	cheese = Cheese.find(params[:id])
-#	cheese.update(params[:cheese])
-#	redirect "/cheeses/#{ cheese.id}"
+	cheese = Cheese.find(params[:id])
+	cheese.update(params[:cheese])
+	redirect "/cheeses/#{ cheese.id }"
 end
 
 
 
 # -DESTROY CHEESESS: destroy -
-delete '/cheese/:id' do
-#	cheese = Cheese.find(params[:id])
-#	redirect '/cheese'
+delete '/cheeses/:id' do
+	Cheese.delete(params[:id])
+	redirect '/cheeses'
 end
